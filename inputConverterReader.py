1614fc0891a95a28a875c81f1f74e26f4144d2c0
John Tompkins <tompkinsjohn04@gmail.com>
2023-05-16 12:03:03 -0500

    add system to require an input from the user for embedding and store it in a variable

--- inputConverterReader.py
+++ inputConverterReader.py
@@ -3,36 +3,67 @@
 #Program takes the data and gives it to other program files to format and convert it correctly
 #Finally, it will be send and stored inside a CSV file
 
+#imports
+import os
+
+#varibles
 textData = "" #holds the data the user puts in the input
 literalStringList = [] #holds a list of the words from the input in string format
 embeddingList = [] #holds a list of the words from the input in converted embedded format
-retry = True #determines if user needs to redo or retree a step if they make a mistake
 userInput = "0" #stores the input given by the user
 
 
 #allow user to pick between selecting a file or typeing text directly
 print("How would you like to enter the text data?")
 print("Press [1] to use file")
-print("Press [2] to use textinput\n")
+print("Press [2] to use text input")
 
 #used to test the input and determine if its a valid input
-while (retry == True):
+while(True):
     try:
         userInput = str(input()).strip()
         if (userInput == "1" or userInput == "2"):
-            retry = False
+            break
         else:
-            print("-Enter a valid input-\n")     
-    except:
-        print("-ERROR: Enter a valid input-\n")
-
-#used for now to show what user selected (wont be in final version)
-if (userInput == "1"):
-    print("\nUSER SELECTED FILE")
-if (userInput == "2"):
-    print("\nUSER SELECTED TYPING")
-
+            print("\n-Enter a valid input-\n")     
+    except Exception as e: 
+            print(e)
     
+    
+#if user selected file, file exploerer will open up 
+#user then selects which file they want to be uses
+if (userInput == "1"):
+    print("\nEnter desired file location for embedding below:")
+    while(True):
+        try:
+            filePath = input() #stores the path to user selected file
+            
+            if (os.path.exists(filePath) and os.path.isfile(filePath)): #makes sure selected file exists and is a file
+                with open(filePath,"r") as file:
+                    textData = file.read() #gets text from file
+                break
+            else:
+                print("\n-Enter valid file for embedding-\n")
+
+        except Exception as e: 
+            print(e)
+
+#if user selects textinput, user types in the text they want to be
+#user then selects which file they want to be uses
+elif (userInput == "2"):
+    print("\nEnter desired text for embedding below:")
+    while(True):
+        try:
+            textData = input() #sotres user inputed text
+            if (textData != ""):
+                break 
+            else:
+                print("\n-Enter text for embedding-\n")
+        except Exception as e: 
+            print(e)
+        
+    
+print("Embedding text: " + textData)
     
 #if user give invalid input, repeat question
 
@@ -62,4 +93,3 @@ if (userInput == "2"):
 
 #user presses enter again once ready to end the program
 input("Press ENTER to exit program")
-
