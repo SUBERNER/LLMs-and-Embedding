cf049247d85f9620be1fd1a2f9d04f31baeaafb5
John Tompkins <tompkinsjohn04@gmail.com>
2023-05-16 09:32:29 -0500

    add pseudocode for main program file

---
+++ inputConverterReader.py
@@ -0,0 +1,48 @@
+#inputConverterReader
+
+#Asks user to select a file
+#Program takes the data and gives it to other program files to format and convert it correctly
+#Finally, it will be send and stored inside a CSV file
+
+
+#allow user to pick between selecting a file or typeing text directly
+    #"How would you like to send your text data?"
+    #"press [1] for file input"
+    #"press [2] for typing input"
+    
+#if user give invalid input, repeat question
+
+#if user selects file input
+    #file exploer is opened and user selects a file to use
+
+#if user selects typing input
+    #program asks user to type the text they want to be processed. Once done the user presses enter
+    #after that the text file is sent to be used
+    
+    
+#Text data from either one is then stored inside of a variable to temperarily hold it until converted correctly
+
+#lists are then created, one sotring the literal string format of the word
+#the other list holds the converted embedded version of the text
+
+#use a foreach loop and convert each word into a embedded version
+#the loop should then add the converted text into the second list
+
+#the embeded list and the literal string lisr should both be simmeterical
+#if the first value is apple in one list, the other list's first value sould be the embedded text of apple
+
+#additionally, use another database to compare the accuracty of the emmbedding used by this program
+
+#then the list is in another foreach loop
+#with each word and corresponding embedding being added in a row in the CVS file
+
+
+
+
+
+
+    
+
+
+
+
