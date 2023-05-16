5af0c33ea9f2c3ef7898617b635a9bd1c7250e23
John Tompkins <tompkinsjohn04@gmail.com>
2023-05-16 10:34:47 -0500

    add basic input system and proram exit capabilities

--- inputConverterReader.py
+++ inputConverterReader.py
@@ -1,14 +1,38 @@
 #inputConverterReader
-
 #Asks user to select a file
 #Program takes the data and gives it to other program files to format and convert it correctly
 #Finally, it will be send and stored inside a CSV file
 
+textData = "" #holds the data the user puts in the input
+literalStringList = [] #holds a list of the words from the input in string format
+embeddingList = [] #holds a list of the words from the input in converted embedded format
+retry = True #determines if user needs to redo or retree a step if they make a mistake
+userInput = "0" #stores the input given by the user
+
 
 #allow user to pick between selecting a file or typeing text directly
-    #"How would you like to send your text data?"
-    #"press [1] for file input"
-    #"press [2] for typing input"
+print("How would you like to enter the text data?")
+print("Press [1] to use file")
+print("Press [2] to use textinput\n")
+
+#used to test the input and determine if its a valid input
+while (retry == True):
+    try:
+        userInput = str(input()).strip()
+        if (userInput == "1" or userInput == "2"):
+            retry = False
+        else:
+            print("-Enter a valid input-\n")     
+    except:
+        print("-ERROR: Enter a valid input-\n")
+
+#used for now to show what user selected (wont be in final version)
+if (userInput == "1"):
+    print("\nUSER SELECTED FILE")
+if (userInput == "2"):
+    print("\nUSER SELECTED TYPING")
+
+    
     
 #if user give invalid input, repeat question
 
@@ -36,13 +60,6 @@
 #then the list is in another foreach loop
 #with each word and corresponding embedding being added in a row in the CVS file
 
-
-
-
-
-
-    
-
-
-
+#user presses enter again once ready to end the program
+input("Press ENTER to exit program")
 
